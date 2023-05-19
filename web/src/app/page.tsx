a30f4017604de003a15efb8ac0ef57c08a19594e
Emanuel <emanunelzsmj@gmail.com>
2023-05-19 16:35:53 -0300

    Autenticacao no Front-end

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,9 @@
+import { cookies } from 'next/headers'
 import Left from '@/components/Left'
 import Right from '@/components/Right'
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
--- web/src/components/Left.tsx
+++ web/src/components/Left.tsx
@@ -1,15 +1,18 @@
+import { cookies } from 'next/headers'
 import { Background } from './Background'
 import Copyright from './Copyright'
 import Hero from './Hero'
 import Signin from './Signin'
+import Profile from './Profile'
 
 export default function Left() {
+  const isAuthenticated = cookies().has('token')
   return (
     <div className="relative flex flex-col items-start justify-between overflow-hidden border-white/10 bg-[url(../assets/stars.svg)] bg-cover p-16 px-28 py-16">
       {/* Blur */} {/* Stripes */}
       <Background />
       {/* SignIn */}
-      <Signin />
+      {isAuthenticated ? <Profile /> : <Signin />}
       {/* Hero */}
       <Hero />
       {/* Copyright */}
