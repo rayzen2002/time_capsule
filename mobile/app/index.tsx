5288528faca9613f0b6adbd3695cb31daf0077e7
Emanuel <emanunelzsmj@gmail.com>
2023-05-19 19:42:50 -0300

    expo router, navegando usuario

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,19 +1,20 @@
 import { StatusBar } from 'expo-status-bar'
-import * as WebBrowser from 'expo-web-browser'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { styled } from 'nativewind'
+import { useEffect } from 'react'
+import { api } from '../src/lib/api'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { useRouter } from 'expo-router'
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NlwLogo from './src/assets/nlw-spacetime-logo.svg'
-import { styled } from 'nativewind'
-import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
-import { useEffect } from 'react'
-import { api } from './src/lib/api'
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NlwLogo from '../src/assets/nlw-spacetime-logo.svg'
+import * as SecureStore from 'expo-secure-store'
 
 const StyledStripes = styled(Stripes)
 const discovery = {
@@ -24,6 +25,7 @@ const discovery = {
 }
 
 export default function App() {
+  const router = useRouter()
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
@@ -39,7 +41,16 @@ export default function App() {
     },
     discovery,
   )
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+    const { token } = response.data
 
+    console.log(token)
+    await SecureStore.setItemAsync('token', token)
+    router.push('/memories')
+  }
   useEffect(() => {
     // console.log(
     //   makeRedirectUri({
@@ -48,18 +59,7 @@ export default function App() {
     // )
     if (response?.type === 'success') {
       const { code } = response.params
-
-      console.log(code)
-
-      api
-        .post('/register', {
-          code,
-        })
-        .then((response) => {
-          const { token } = response.data
-
-          console.log(token)
-        })
+      handleGithubOAuthCode(code)
     }
   }, [response])
 
