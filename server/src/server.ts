79ac772890737da541013c5aef63869e3df3faf5
Emanuel <emanunelzsmj@gmail.com>
2023-05-22 16:47:44 -0300

    Rota de upload de arquivos

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,20 @@ import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import 'dotenv/config'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
 
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+
+})
+app.register(multipart)
 app.register(cors, {
   origin: true,
 })
@@ -14,6 +23,7 @@ app.register(jwt, {
   secret: 'spacetime',
 })
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 app.register(authRoutes)
 
 app
