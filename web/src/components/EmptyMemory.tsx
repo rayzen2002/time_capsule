f1102ef1f2734fbb6e239c711daab728b346c670
Emanuel <emanunelzsmj@gmail.com>
2023-05-24 14:55:42 -0300

    files to readme

--- web/src/components/EmptyMemory.tsx
+++ web/src/components/EmptyMemory.tsx
@@ -1,15 +1,13 @@
 export function EmptyMemorie() {
   return (
-    <div>
-      <div className="flex flex-1 items-center justify-center p-16">
-        <p className="w-[360px] text-center leading-relaxed">
-          Você ainda não registrou nenhuma lembrança, começa{' '}
-          <a href="" className="underline hover:text-gray-50">
-            criar agora
-          </a>
-          !
-        </p>
-      </div>
+    <div className="flex flex-1 items-center justify-center p-16">
+      <p className="w-[360px] text-center leading-relaxed">
+        Você ainda não registrou nenhuma lembrança, começa{' '}
+        <a href="" className="underline hover:text-gray-50">
+          criar agora
+        </a>
+        !
+      </p>
     </div>
   )
 }
